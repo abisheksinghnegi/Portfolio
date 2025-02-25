e51bef98b0626f57f10c9d52bc3aa6dca40df2ce
abisheksinghnegi <abhishekofficial1910@gmail.com>
2025-02-26 00:57:42 +0530

    commit

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,7 +5,6 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Project";
 import Contact from "./components/Contact"; 
-import Footer from "./components/Footer"; 
 import About2 from "./components/About2";
 import Contact2 from "./components/Contact2";
 
@@ -22,7 +21,7 @@ function App() {
       <Projects />
       <Contact2 />
       <Contact />
-      <Footer/>
+
     </div>
   );
 }
